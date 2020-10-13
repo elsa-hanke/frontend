3f32ab0085536fd0743cdb6f702f33a8c9b3dde0
Sauli Anto <sauli.anto@gofore.com>
2020-10-13 19:09:03 +0300

    Lisätty ikoneita

---
+++ src/configs/fontawesome-solid.ts
@@ -0,0 +1,42 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faArrowsAlt,
+  faArrowUp,
+  faAward,
+  faCheckCircle,
+  faCircle,
+  faClipboard,
+  faClipboardList,
+  faEnvelope,
+  faFile,
+  faFileAlt,
+  faHome,
+  faInfo,
+  faInfoCircle,
+  faPaperPlane,
+  faPortrait,
+  faQuestion,
+  faQuestionCircle,
+  faSearch,
+  faShare
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faArrowsAlt);
+library.add(faArrowUp);
+library.add(faAward);
+library.add(faCheckCircle);
+library.add(faCircle);
+library.add(faClipboard);
+library.add(faClipboardList);
+library.add(faEnvelope);
+library.add(faFile);
+library.add(faFileAlt);
+library.add(faHome);
+library.add(faInfo);
+library.add(faInfoCircle);
+library.add(faPaperPlane);
+library.add(faPortrait);
+library.add(faQuestion);
+library.add(faQuestionCircle);
+library.add(faSearch);
+library.add(faShare);
