aa0c5760e4870096b0bd3e3b0e46235a1f484bcc
Sauli Anto <sauli.anto@gofore.com>
2020-10-13 19:09:03 +0300

    Lisätty ikoneita

---
+++ src/configs/fontawesome-regular.ts
@@ -0,0 +1,5 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCalendarAlt, faCircle } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faCalendarAlt);
+library.add(faCircle);
--- src/configs/fontawesome.ts
+++ src/configs/fontawesome.ts
@@ -1,48 +1,12 @@
 import Vue from "vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faHome,
-  faFile,
-  faSearch,
-  faAward,
-  faEnvelope,
-  faFileAlt,
-  faCalendar,
-  faArrowsAlt,
-  faCircle,
-  faPortrait,
-  faCheckCircle,
-  faClipboard,
-  faClipboardList,
-  faArrowUp,
-  faPaperPlane,
-  faShare,
-  faQuestionCircle,
-  faQuestion
-} from "@fortawesome/free-solid-svg-icons";
+
+import "./fontawesome-solid";
+import "./fontawesome-regular";
+
 import {
   FontAwesomeIcon,
   FontAwesomeLayers
 } from "@fortawesome/vue-fontawesome";
-
-library.add(faHome);
-library.add(faFile);
-library.add(faSearch);
-library.add(faAward);
-library.add(faEnvelope);
-library.add(faFileAlt);
-library.add(faCalendar);
-library.add(faArrowsAlt);
-library.add(faCircle);
-library.add(faPortrait);
-library.add(faCheckCircle);
-library.add(faClipboard);
-library.add(faClipboardList);
-library.add(faArrowUp);
-library.add(faPaperPlane);
-library.add(faShare);
-library.add(faQuestionCircle);
-library.add(faQuestion);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 Vue.component("font-awesome-layers", FontAwesomeLayers);
