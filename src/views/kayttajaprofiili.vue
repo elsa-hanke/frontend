6e9eaab9507ef16dc6478757971d443ea200b116
Sauli Anto <sauli.anto@gofore.com>
2020-10-22 23:55:24 +0300

    Toteutettu käyttäjäprofiilinäkymää

---
+++ src/views/kayttajaprofiili.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="itsearviointi">
+    <b-breadcrumb :items="items" class="mb-0"></b-breadcrumb>
+    <b-container fluid>
+      <b-row lg>
+        <b-col class="px-0">
+          <b-card-skeleton
+            :loading="false"
+            class="mb-3 itsearviointi-card"
+            :header="$t('tee-itsearviointi')"
+          >
+            Käyttäjäprofiili - oma rooli - opiskeluoikeudet
+          </b-card-skeleton>
+        </b-col>
+        <b-col class="pl-3 pr-0" lg="2"></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {}
+})
+export default class Kayttajaprofiili extends Vue {}
+</script>
