099696d08b13c4a6f326cbe233fc0c1d30370ea2
Sauli Anto <sauli.anto@gofore.com>
2020-11-03 12:06:46 +0200

    Toteutettu arvioinnin näkymää

--- src/components/luottamuksen-taso/luottamuksen-taso.vue
+++ src/components/luottamuksen-taso/luottamuksen-taso.vue
@@ -13,39 +13,19 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
+import { luottamuksenTasot } from "@/utils/constants";
 
 @Component({})
 export default class ElsaLuottamuksenTaso extends Vue {
   @Prop({ required: true })
   value!: number;
 
-  tasot = [
-    {
-      arvo: 1,
-      kuvaus: this.$t("ohjaajan-toiminnan-seuraaminen")
-    },
-    {
-      arvo: 2,
-      kuvaus: this.$t("toiminta-suoran-ohjauksen-alaisena")
-    },
-    {
-      arvo: 3,
-      kuvaus: this.$t("toiminta-epasuoran-ohjauksen-alaisena")
-    },
-    {
-      arvo: 4,
-      kuvaus: this.$t("toiminta-ilman-ohjausta")
-    },
-    {
-      arvo: 5,
-      kuvaus: this.$t("toiminta-ohjaajana")
-    }
-  ];
+  tasot = luottamuksenTasot;
 
   get tasonKuvaus() {
     const taso = this.tasot.find(taso => taso.arvo === this.value);
     if (taso) {
-      return taso.kuvaus;
+      return this.$t(taso.kuvaus);
     }
     return undefined;
   }
---
+++ src/utils/constants.ts
@@ -0,0 +1,45 @@
+export const vaativuustasot = [
+  {
+    arvo: 1,
+    kuvaus: "aloittelija"
+  },
+  {
+    arvo: 2,
+    kuvaus: "helppo"
+  },
+  {
+    arvo: 3,
+    kuvaus: "kohtalainen"
+  },
+  {
+    arvo: 4,
+    kuvaus: "vaativa"
+  },
+  {
+    arvo: 5,
+    kuvaus: "hyvin-vaativa"
+  }
+] as const;
+
+export const luottamuksenTasot = [
+  {
+    arvo: 1,
+    kuvaus: "ohjaajan-toiminnan-seuraaminen"
+  },
+  {
+    arvo: 2,
+    kuvaus: "toiminta-suoran-ohjauksen-alaisena"
+  },
+  {
+    arvo: 3,
+    kuvaus: "toiminta-epasuoran-ohjauksen-alaisena"
+  },
+  {
+    arvo: 4,
+    kuvaus: "toiminta-ilman-ohjausta"
+  },
+  {
+    arvo: 5,
+    kuvaus: "toiminta-ohjaajana"
+  }
+] as const;
