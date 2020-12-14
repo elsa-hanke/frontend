8b5d0b1ddc37580d5dc3ac6beafcf8a5ba2e682a
Sauli Anto <sauli.anto@gofore.com>
2020-12-14 18:19:52 +0200

    UOELSA-176: Toteutettu työskentelyaijan laskemista

--- src/components/progress-bar/progress-bar.vue
+++ src/components/progress-bar/progress-bar.vue
@@ -1,13 +1,15 @@
 <template>
   <div
-    class="progress mb-1"
+    class="progress"
     :style="`height: 1.5rem; background-color: ${backgroundColor}`"
   >
     <div
       class="progress-bar"
-      :style="`width: ${value}%; background-color: ${color}`"
+      :style="`width: ${ratio}%; background-color: ${color}`"
     >
-      <span class="position-absolute font-weight-500 px-2">3 kk 2 pv</span>
+      <span class="position-absolute font-weight-500 text-size-sm px-2">{{
+        $duration(value)
+      }}</span>
     </div>
   </div>
 </template>
@@ -16,16 +18,24 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
+import { clamp } from "@/utils/functions";
 
 @Component
 export default class ElsaProgressBar extends Vue {
   @Prop({ required: true })
   value!: number;
 
+  @Prop({ required: true })
+  minRequired!: number;
+
   @Prop({ required: false })
   color!: string;
 
   @Prop({ required: false })
   backgroundColor!: string;
+
+  get ratio() {
+    return clamp((this.value / this.minRequired) * 100, 0, 100);
+  }
 }
 </script>
