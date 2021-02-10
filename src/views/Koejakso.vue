b1287720dbcf4d14d31fa45f05f831bd734d2fac
Tomi Tolvanen <tomi.t.tolvanen@gmail.com>
2021-02-10 13:02:58 +0200

    Lisätty elementtejä.

--- src/views/Koejakso.vue
+++ src/views/Koejakso.vue
@@ -5,12 +5,38 @@
       <b-row lg>
         <b-col class="px-0">
           <h1>{{ $t("koejakso") }}</h1>
-          <p>{{ $t("koejakso-kuvaus") }}</p>
+          <p>
+            {{ $t("koejakso-kuvaus") }}
+            <b-link href=""> {{ $t("koejakso-tavoitteet-linkki") }}</b-link>
+          </p>
           <div class="d-flex justify-content-center border rounded pt-3 mb-4">
             <div class="container-fluid">
-              <elsa-form-group :label="$t('tyoskentelyaika-erikoisalalla')">
-                <h2>{{ $t("koejakson-suorituspaikka") }}</h2>
-                <p>{{ $t("koejakson-suorituspaikka-kuvaus") }}</p>
+              <h2>{{ $t("koejakson-suorituspaikka") }}</h2>
+              <p>{{ $t("koejakson-suorituspaikka-kuvaus") }}</p>
+              <p>
+                <b-link>
+                  {{ $t("lisää-työskentelyjakso") }}
+                </b-link>
+              </p>
+              <elsa-form-group :label="$t('tyoskentelyjakso')">
+                <elsa-button
+                  variant="primary"
+                  :to="{ name: 'arviointipyynto' }"
+                  class="mb-4"
+                >
+                  {{ $t("liitä-koejaksoon") }}
+                </elsa-button>
+                <template v-slot="{ uid }">
+                  <elsa-form-multiselect
+                    :id="uid"
+                    v-model="selected.tyoskentelyjakso"
+                    :options="tyoskentelyjaksotFormatted"
+                    label="label"
+                    track-by="id"
+                    @select="onTyoskentelyjaksoSelect"
+                  >
+                  </elsa-form-multiselect>
+                </template>
               </elsa-form-group>
             </div>
           </div>
@@ -136,15 +162,30 @@ import { Component, Vue } from "vue-property-decorator";
 import ElsaButton from "@/components/button/button.vue";
 import ElsaPopover from "@/components/popover/popover.vue";
 import ElsaLuottamuksenTaso from "@/components/luottamuksen-taso/luottamuksen-taso.vue";
+import { tyoskentelyjaksoLabel } from "@/utils/tyoskentelyjakso";
+import ElsaFormGroup from "@/components/form-group/form-group.vue";
+import ElsaFormMultiselect from "@/components/multiselect/multiselect.vue";
 
 @Component({
   components: {
     ElsaButton,
     ElsaPopover,
-    ElsaLuottamuksenTaso
+    ElsaLuottamuksenTaso,
+    ElsaFormGroup,
+    ElsaFormMultiselect
   }
 })
 export default class Koejakso extends Vue {
+  selected = {
+    tyoskentelyjakso: null,
+    epaOsaamisalue: null,
+    kouluttaja: null
+  } as any;
+  options = {
+    tyoskentelyjaksot: [],
+    epaOsaamisalueet: [],
+    kouluttajat: []
+  } as any;
   items = [
     {
       text: this.$t("etusivu"),
@@ -157,10 +198,45 @@ export default class Koejakso extends Vue {
   ];
 
   async mounted() {
+    console.log("mounted()...");
     const oppimistavoitteet = (
       await axios.get("erikoistuva-laakari/oppimistavoitteet-taulukko")
     ).data;
     console.log(oppimistavoitteet.toString());
+  }
+
+  get tyoskentelyjaksotFormatted() {
+    console.log("tyoskentelyjaksotFormatted()...");
+    return this.options.tyoskentelyjaksot.map((tj: any) => ({
+      ...tj,
+      label: tyoskentelyjaksoLabel(this, tj)
+    }));
+  }
+
+  async onTyoskentelyjaksoSelect(selected: any) {
+    console.log("onTyoskentelyjaksoSelect()...");
+    this.selected.tyoskentelyjakso = selected;
+    await this.fetch();
+  }
+
+  async fetch(options: any = {}) {
+    try {
+      const res = (
+        await axios.get("erikoistuva-laakari/suoritusarvioinnit", {
+          params: {
+            ...options,
+            sort: "tapahtumanAjankohta,desc",
+            "tyoskentelyjaksoId.equals": this.selected.tyoskentelyjakso?.id,
+            "arvioitavaOsaalueId.equals": this.selected.epaOsaamisalue?.id,
+            "arvioinninAntajaId.equals": this.selected.kouluttaja?.id
+          }
+        })
+      ).data;
+      console.log("xxx:" + res.toString());
+      //this.kategoriat = this.solveKategoriat();
+    } catch (err) {
+      //this.omat = [];
+    }
   }
 }
 </script>
