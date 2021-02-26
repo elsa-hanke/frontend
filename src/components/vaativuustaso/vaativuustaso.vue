4061a3a87c5f78892355d6b7aac7d0fc15ea6656
Arttu Hänninen <arttu.hanninen@gofore.com>
2021-02-26 11:17:33 +0200

    UOELSA-92: Korjattu arvioinnin perustumisen validaatio ja lisätty vaativuustasoille selitteet

---
+++ src/components/vaativuustaso/vaativuustaso.vue
@@ -0,0 +1,31 @@
+<template>
+  <elsa-badge :value="value" :help="tasonNimi" />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+import { vaativuustasot } from "@/utils/constants";
+import ElsaBadge from "@/components/badge/badge.vue";
+
+@Component({
+  components: {
+    ElsaBadge
+  }
+})
+export default class ElsaVaativuustaso extends Vue {
+  @Prop({ required: true })
+  value!: number;
+
+  tasot = vaativuustasot;
+
+  get tasonNimi() {
+    const taso = this.tasot.find(taso => taso.arvo === this.value);
+    if (taso) {
+      return this.$t(taso.nimi);
+    }
+    return undefined;
+  }
+}
+</script>
