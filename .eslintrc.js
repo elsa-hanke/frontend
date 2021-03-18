9daad5499319af5d4eba154bc55f7015459a4943
ovarr <otto.varrela@gofore.com>
2021-03-18 16:54:48 +0200

    prettier lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended',
-    '@vue/prettier',
-    '@vue/prettier/@typescript-eslint'
-  ],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/typescript/recommended', '@vue/prettier', '@vue/prettier/@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020
   },
@@ -17,18 +11,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': [
-      'warn',
-      {
-        printWidth: 200,
-        trailingComma: 'none',
-        semi: false,
-        singleQuote: true,
-        indent: [2, 2, {SwitchCase: 1}],
-        vueIndentScriptAndStyle: true,
-        htmlWhitespaceSensitivity: 'ignore'
-      }
-    ],
     'vue/script-indent': [
       'error',
       2,
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,1 +1,9 @@
-module.exports = {}
+module.exports = {
+  printWidth: 200,
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  indent: [2, 2, { SwitchCase: 1 }],
+  vueIndentScriptAndStyle: true,
+  htmlWhitespaceSensitivity: 'ignore'
+}
