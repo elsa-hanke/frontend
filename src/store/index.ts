6a788899c11fd378c0256f45052991f0280f2f2f
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-06-16 15:39:57 +0300

    UOELSA-508: Vastuuhenkilön koejakso
    - Lisää api koejakson vaiheiden hakemiselle
    - Lisää store vastuuhenkilön koejaksoille

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 import auth from './auth'
 import erikoistuva from './erikoistuva'
 import kouluttaja from './kouluttaja'
+import vastuuhenkilo from './vastuuhenkilo'
 
 Vue.use(Vuex)
 
@@ -11,6 +12,7 @@ export default new Vuex.Store({
   modules: {
     auth,
     erikoistuva,
-    kouluttaja
+    kouluttaja,
+    vastuuhenkilo
   }
 })
