04cb87e6c29fece8034ba450c1c8a0d536ee315c
Tero Viljakainen <tero.viljakainen@gofore.com>
2021-06-23 18:07:08 +0300

    UOELSA-558: Lisää null check kouluttajatAndVastuuhenkilot listalle

--- src/utils/arvioinninAntajaListDecorator.ts
+++ src/utils/arvioinninAntajaListDecorator.ts
@@ -1,7 +1,7 @@
 import { ELSA_ROLE } from './roles'
 
 export function decorate(vue: Vue, kouluttajatAndVastuuhenkilot: any[]) {
-  return kouluttajatAndVastuuhenkilot.map((item: any) => {
+  return kouluttajatAndVastuuhenkilot?.map((item: any) => {
     if (item.authorities.some((a: any) => a.name === ELSA_ROLE.Vastuuhenkilo)) {
       const newItem = Object.assign({}, item)
       newItem.nimi = `${item.nimi} (${vue.$t('vastuuhenkilo')})`
